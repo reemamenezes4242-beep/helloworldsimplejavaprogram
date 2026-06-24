0e0fc334e0b230c95f3141dcd0a8d4eccd7fbc24
reemamenezes4242-beep <reemamenezes4242@gmail.com>
2026-06-24 11:39:53 +0530

    Fix class name typo in helloworld.java
    Corrected the class name from 'hellowrorld' to 'helloworld'.

--- helloworld.java
+++ helloworld.java
@@ -1,4 +1,4 @@
-class hellowrorld{  
+class helloworld{  
     public static void main(String args[]){  
      System.out.println("Hello batch43 team with  Mohit, Yogesh , Sachoin");
       System.out.println("Hello Batch batch42 WITH FOR STUDENTS RUNNING in front of me and Anurag Prince in making CI automated vikas teach devops CI proecss");
